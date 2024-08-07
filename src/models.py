1eb7c8f6e16e1add67abe08a22944347a4c55391
Cesar Amaya Colella <cesar.rafael.gep@gmail.com>
2024-08-07 20:58:56 +0000

    some changes to the favorites class

--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
+from sqlalchemy.sql import func
 
 db = SQLAlchemy()
 
@@ -8,18 +9,19 @@ class User(db.Model):
     username = db.Column(db.String(250), nullable=False, unique=True)
     email = db.Column(db.String(250), nullable=False, unique=True)
     password_hash = db.Column(db.String(250), nullable=False)
+    favorites = db.relationship("Favorite", backref="user")
 
     def __init__(self, username, email, password):
         self.username = username
         self.email = email
         self.set_password(password)
         db.session.add(self)
-        try: 
+        try:
             db.session.commit()
         except Exception as error:
             db.session.rollback()
             raise Exception(error.args)
-        
+
     def __repr__(self):
         return '<User %r>' % self.username
 
@@ -34,17 +36,25 @@ class User(db.Model):
             "id": self.id,
             "username": self.username,
             "email": self.email,
+            "favorites": [favorite.serialize() for favorite in self.favorites]
         }
 
 class Character(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(250))
+    favorites = db.relationship("Favorite", backref="character")
 
     def __init__(self, name, description=None):
         self.name = name
         self.description = description
-    
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args)
+
     def serialize(self):
         return {
             "id": self.id,
@@ -56,11 +66,18 @@ class Planet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(250), nullable=False)
     description = db.Column(db.String(250))
+    favorites = db.relationship("Favorite", backref="planet")
 
     def __init__(self, name, description=None):
         self.name = name
         self.description = description
-    
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args)
+
     def serialize(self):
         return {
             "id": self.id,
@@ -70,23 +87,27 @@ class Planet(db.Model):
 
 class Favorite(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime(timezone=True), default=func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     character_id = db.Column(db.Integer, db.ForeignKey('character.id'), nullable=True)
     planet_id = db.Column(db.Integer, db.ForeignKey('planet.id'), nullable=True)
-
-    user = db.relationship("User", backref="favorites")
-    character = db.relationship("Character")
-    planet = db.relationship("Planet")
 
     def __init__(self, user_id, character_id=None, planet_id=None):
         self.user_id = user_id
         self.character_id = character_id
         self.planet_id = planet_id
-    
+        db.session.add(self)
+        try:
+            db.session.commit()
+        except Exception as error:
+            db.session.rollback()
+            raise Exception(error.args)
+
     def serialize(self):
         return {
             "id": self.id,
             "user_id": self.user_id,
             "character_id": self.character_id,
-            "planet_id": self.planet_id
+            "planet_id": self.planet_id,
+            "date": self.date.isoformat()
         }
